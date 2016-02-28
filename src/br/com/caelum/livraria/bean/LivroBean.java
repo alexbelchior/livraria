bd0b2b9fc25718fd394cf9d171869fdf50a477d1
Alexandre Belchior Medeiros <alexbelchior@gmail.com>
2016-02-27 22:30:19 -0300

    Aula 5

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,13 +1,17 @@
 package br.com.caelum.livraria.bean;
 
-import br.com.caelum.livraria.modelo.*;
-
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
 @ViewScoped
@@ -51,10 +55,20 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	}
+	
+	public void comecaComUm(FacesContext fc, UIComponent component, Object value)throws ValidatorException{
+		
+		String valor = value.toString();
+		if(!valor.startsWith("1")){
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+		
 	}
 
 }
